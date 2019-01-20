d3a6b26eede0aaab8bcf69a973766d6df3dc9cd2
hardikshakya <hardikshakya1997@gmail.com>
2019-01-20 14:57:40 +0530

    Add POST test and GET /todos

---
+++ server/tests/server.test.js
@@ -0,0 +1,34 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+    it('should crete a new todo', (done) => {
+        var text = 'Test todo text';
+
+        request(app)
+            .post('/todos')
+            .send({text})
+            .expect(200)
+            .expect(() => {
+                expect(res.body.text).toBe(text);
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
