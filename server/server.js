dbfd163a870e3d74a1a430e43816985479b68ada
hardikshakya <hardikshakya1997@gmail.com>
2019-01-20 16:34:39 +0530

    Try to deploy

--- server/server.js
+++ server/server.js
@@ -76,6 +76,7 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -123,8 +124,8 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log('Sarted on port 3000');    
+app.listen(port, () => {
+    console.log(`Sarted up at port ${port}`);    
 });
 
 module.exports = {
