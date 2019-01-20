69b496f006da1fc19a3cec9f2f28ec86ecb42fb4
hardikshakya <hardikshakya1997@gmail.com>
2019-01-20 19:01:38 +0530

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -67,8 +67,9 @@
 //     console.log('Unable to save user', e);    
 // });
 
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
@@ -141,6 +142,31 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(404).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();       
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(port, () => {
