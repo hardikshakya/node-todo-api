39d91dedd1253f26ca3bf442e74d1802ad68214e
hardikshakya <hardikshakya1997@gmail.com>
2019-01-20 15:58:25 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -69,6 +69,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -98,6 +99,28 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    // res.send(req.params);
+
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+       return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();       
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(404).send();
+    });
+    
+
 });
 
 app.listen(3000, () => {
