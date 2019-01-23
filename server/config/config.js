1e9e64d874bbc4e4ccfd09d5792ca2279857c90f
Hardik Shakya <hardikshakya1997@gmail.com>
2019-01-23 18:30:47 +0530

    Update config to use json file

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  // console.log(config);
+
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+  
+}
